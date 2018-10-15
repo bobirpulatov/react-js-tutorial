88fe25258f515df15878ead44eb24b53f1ef5869
BobSteel <bobir_1997@mail.ru>
2018-10-16 00:07:44 +0500

    Finished HT 3

--- src/js/Articles/Comments/CommentList.js
+++ src/js/Articles/Comments/CommentList.js
@@ -2,19 +2,15 @@ import React, {Component} from 'react';
 import Comment from "./Comment";
 import ToggleOpen from "../../Decorators/ToggleOpen";
 import PropTypes from 'prop-types';
-
+import AddNewComment from './../../Forms/AddNewComment';
 
 class CommentList extends Component{
-   componentWillMount(){ console.log('Component will mount'); }
-   componentDidMount(){ console.log('Component did mount'); }
-
-   componentWillUnmount(){ console.log('Component will unmount'); }
-
    render(){
       const {isOpen, toggleOpen} = this.props;
 
       return (
-         <div className="comments-container">
+         <div className="comments-container"><br/>
+          <AddNewComment />
             <button onClick={toggleOpen}>{ (isOpen) ? 'Hide comments' : 'Show comments' }</button>
             { (isOpen) ? this.getBody() : '' }
          </div>
--- src/js/app.js
+++ src/js/app.js
@@ -11,8 +11,6 @@ class App extends Component{
 
       return (
          <div className="wrapper">
-            <UserForm />
-            <DatePicker />
             <ArticleList />
          </div>
       );
