63035d5b896502d682baa28bd8192fa07c195932
Bob Steel <bogdan_9726@mail.ru>
2018-10-15 17:38:13 +0500

    Finished 3rd lesson but not homework

--- src/js/Articles/Article.js
+++ src/js/Articles/Article.js
@@ -1,6 +1,9 @@
 import React, {Component, PureComponent} from 'react';
 import CommentList from './Comments/CommentList';
 import PropTypes from "prop-types";
+import { CSSTransition } from 'react-transition-group'
+
+import './../../css/style.css';
 
 class Article extends PureComponent{
    static propTypes = {
@@ -18,14 +21,16 @@ class Article extends PureComponent{
 
    render(){
       const {isOpen, toggleOpen, article} = this.props;
-      const {id, title, date} = article;
+      const {title, date} = article;
 
       return (
          <div className="each-article-container">
             <h4>{title} &mdash; <small>{new Date(date).toLocaleTimeString()}</small></h4>
             <button className="article-toggler" onClick={ toggleOpen }>
                { (isOpen) ? 'Hide article' : 'Show article' }</button>
-            {this.getBody()}
+            <CSSTransition in={isOpen} timeout={2000} classNames="fade">
+               {this.getBody()}
+            </CSSTransition>
          </div>
       );
    }
@@ -34,11 +39,11 @@ class Article extends PureComponent{
       const { text, comments } = article;
       return (isOpen)
          ?
-         <section className="article-text-container">
+         <section>
             { text }
             <CommentList comments={ comments } />
          </section>
-         : ''
+         : <section>&nbsp;</section>
    }
 
 }
